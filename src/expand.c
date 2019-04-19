c6216640e495a370cd31952d8c5c956715c81cc9
marty-emmanuel <brothers@puzzlebrothers.com>
2019-04-19 20:57:02 +0200

    Speed in-tool decompressor further

--- src/expand.c
+++ src/expand.c
@@ -34,7 +34,7 @@
 #define FORCE_INLINE __attribute__((always_inline))
 #endif /* _MSC_VER */
 
-static inline FORCE_INLINE int lz4ultra_expand_literals_slow(const unsigned char **ppInBlock, const unsigned char *pInBlockEnd, int nLiterals, unsigned char **ppCurOutData, const unsigned char *pOutDataEnd) {
+static inline FORCE_INLINE int lz4ultra_expand_literals_slow(const unsigned char **ppInBlock, const unsigned char *pInBlockEnd, unsigned int nLiterals, unsigned char **ppCurOutData, const unsigned char *pOutDataEnd) {
    const unsigned char *pInBlock = *ppInBlock;
    unsigned char *pCurOutData = *ppCurOutData;
 
@@ -42,21 +42,26 @@ static inline FORCE_INLINE int lz4ultra_expand_literals_slow(const unsigned char
       unsigned char nByte;
 
       do {
-         if (pInBlock >= pInBlockEnd) break;
-         nByte = *pInBlock++;
-         nLiterals += (int)((unsigned int)nByte);
+         if (pInBlock < pInBlockEnd) {
+            nByte = *pInBlock++;
+            nLiterals += ((unsigned int)nByte);
+         }
+         else {
+            break;
+         }
       } while (nByte == 255);
    }
 
    if (nLiterals != 0) {
-      if ((pInBlock + nLiterals) > pInBlockEnd ||
-         (pCurOutData + nLiterals) > pOutDataEnd) {
+      if ((pInBlock + nLiterals) <= pInBlockEnd &&
+         (pCurOutData + nLiterals) <= pOutDataEnd) {
+         memcpy(pCurOutData, pInBlock, nLiterals);
+         pInBlock += nLiterals;
+         pCurOutData += nLiterals;
+      }
+      else {
          return -1;
       }
-
-      memcpy(pCurOutData, pInBlock, nLiterals);
-      pInBlock += nLiterals;
-      pCurOutData += nLiterals;
    }
 
    *ppInBlock = pInBlock;
@@ -64,7 +69,7 @@ static inline FORCE_INLINE int lz4ultra_expand_literals_slow(const unsigned char
    return 0;
 }
 
-static inline FORCE_INLINE int lz4ultra_expand_match_slow(const unsigned char **ppInBlock, const unsigned char *pInBlockEnd, const unsigned char *pSrc, int nMatchLen, unsigned char **ppCurOutData, const unsigned char *pOutDataEnd, const unsigned char *pOutDataFastEnd) {
+static inline FORCE_INLINE int lz4ultra_expand_match_slow(const unsigned char **ppInBlock, const unsigned char *pInBlockEnd, const unsigned char *pSrc, unsigned int nMatchLen, unsigned char **ppCurOutData, const unsigned char *pOutDataEnd, const unsigned char *pOutDataFastEnd) {
    const unsigned char *pInBlock = *ppInBlock;
    unsigned char *pCurOutData = *ppCurOutData;
 
@@ -72,60 +77,48 @@ static inline FORCE_INLINE int lz4ultra_expand_match_slow(const unsigned char **
       unsigned char nByte;
 
       do {
-         if (pInBlock >= pInBlockEnd) break;
-         nByte = *pInBlock++;
-         nMatchLen += (int)((unsigned int)nByte);
+         if (pInBlock < pInBlockEnd) {
+            nByte = *pInBlock++;
+            nMatchLen += ((unsigned int)nByte);
+         }
+         else {
+            break;
+         }
       } while (nByte == 255);
    }
 
    nMatchLen += MIN_MATCH_SIZE;
 
-   if ((pCurOutData + nMatchLen) > pOutDataEnd) {
-      return -1;
-   }
+   if ((pCurOutData + nMatchLen) <= pOutDataEnd) {
+      if ((pCurOutData - pSrc) >= 8 && (pCurOutData + nMatchLen) <= pOutDataFastEnd) {
+         const unsigned char *pCopySrc = pSrc;
+         unsigned char *pCopyDst = pCurOutData;
+         const unsigned char *pCopyEndDst = pCurOutData + nMatchLen;
 
-   if ((pSrc + 1) == pCurOutData && nMatchLen >= 16) {
-      /* One-byte RLE */
-      memset(pCurOutData, *pSrc, nMatchLen);
-      pCurOutData += nMatchLen;
-   }
-   else {
-      /* Do a deterministic, left to right byte copy instead of memcpy() so as to handle overlaps */
+         do {
+            memcpy(pCopyDst, pCopySrc, 8);
+            memcpy(pCopyDst + 8, pCopySrc + 8, 8);
+            pCopySrc += 16;
+            pCopyDst += 16;
+         } while (pCopyDst < pCopyEndDst);
 
-      if ((pCurOutData - pSrc) >= 8) {
-         int nMaxFast = nMatchLen;
-         if ((pCurOutData + nMaxFast) > (pOutDataFastEnd - 15))
-            nMaxFast = (int)(pOutDataFastEnd - 15 - pCurOutData);
-
-         if (nMaxFast > 0) {
-            const unsigned char *pCopySrc = pSrc;
-            unsigned char *pCopyDst = pCurOutData;
-            const unsigned char *pCopyEndDst = pCurOutData + nMaxFast;
-
-            do {
-               memcpy(pCopyDst, pCopySrc, 8);
-               memcpy(pCopyDst + 8, pCopySrc+8, 8);
-               pCopySrc += 16;
-               pCopyDst += 16;
-            } while (pCopyDst < pCopyEndDst);
-
-            pCurOutData += nMaxFast;
-            pSrc += nMaxFast;
-            nMatchLen -= nMaxFast;
+         pCurOutData += nMatchLen;
+      }
+      else {
+         while (nMatchLen >= 4) {
+            *pCurOutData++ = *pSrc++;
+            *pCurOutData++ = *pSrc++;
+            *pCurOutData++ = *pSrc++;
+            *pCurOutData++ = *pSrc++;
+            nMatchLen -= 4;
+         }
+         while (nMatchLen--) {
+            *pCurOutData++ = *pSrc++;
          }
       }
-
-      while (nMatchLen >= 4) {
-         *pCurOutData++ = *pSrc++;
-         *pCurOutData++ = *pSrc++;
-         *pCurOutData++ = *pSrc++;
-         *pCurOutData++ = *pSrc++;
-         nMatchLen -= 4;
-      }
-      while (nMatchLen > 0) {
-         *pCurOutData++ = *pSrc++;
-         nMatchLen--;
-      }
+   }
+   else {
+      return -1;
    }
 
    *ppInBlock = pInBlock;
@@ -149,15 +142,15 @@ int lz4ultra_expand_block(const unsigned char *pInBlock, int nBlockSize, unsigne
    const unsigned char *pInBlockFastEnd = pInBlock + nBlockSize - 16;
    unsigned char *pCurOutData = pOutData + nOutDataOffset;
    const unsigned char *pOutDataEnd = pCurOutData + nBlockMaxSize;
-   const unsigned char *pOutDataFastEnd = pOutDataEnd - 16;
+   const unsigned char *pOutDataFastEnd = pOutDataEnd - 20;
 
    /* Fast loop */
 
    while (pInBlock < pInBlockFastEnd && pCurOutData < pOutDataFastEnd) {
       const unsigned char token = *pInBlock++;
-      int nLiterals = (int)((unsigned int)((token & 0xf0) >> 4));
+      unsigned int nLiterals = (unsigned int)((token & 0xf0) >> 4);
 
-      if (nLiterals < 15) {
+      if (nLiterals < LITERALS_RUN_LEN) {
          memcpy(pCurOutData, pInBlock, 16);
          pInBlock += nLiterals;
          pCurOutData += nLiterals;
@@ -174,17 +167,21 @@ int lz4ultra_expand_block(const unsigned char *pInBlock, int nBlockSize, unsigne
          nMatchOffset |= (((unsigned int)*pInBlock++) << 8);
 
          const unsigned char *pSrc = pCurOutData - nMatchOffset;
-         if (pSrc < pOutData)
-            return -1;
-
-         int nMatchLen = (int)((unsigned int)(token & 0x0f));
-         if (nMatchLen < (16 - MIN_MATCH_SIZE + 1) && (pSrc + MIN_MATCH_SIZE + nMatchLen) < pCurOutData && pCurOutData < pOutDataFastEnd) {
-            memcpy(pCurOutData, pSrc, 16);
-            pCurOutData += (MIN_MATCH_SIZE + nMatchLen);
+         if (pSrc >= pOutData) {
+            unsigned int nMatchLen = (unsigned int)(token & 0x0f);
+            if (nMatchLen < MATCH_RUN_LEN && nMatchOffset >= 8 && pCurOutData < pOutDataFastEnd) {
+               memcpy(pCurOutData, pSrc, 8);
+               memcpy(pCurOutData + 8, pSrc + 8, 8);
+               memcpy(pCurOutData+16, pSrc+16, 4);
+               pCurOutData += (MIN_MATCH_SIZE + nMatchLen);
+            }
+            else {
+               if (lz4ultra_expand_match_slow(&pInBlock, pInBlockEnd, pSrc, nMatchLen, &pCurOutData, pOutDataEnd, pOutDataFastEnd))
+                  return -1;
+            }
          }
          else {
-            if (lz4ultra_expand_match_slow(&pInBlock, pInBlockEnd, pSrc, nMatchLen, &pCurOutData, pOutDataEnd, pOutDataFastEnd))
-               return -1;
+            return -1;
          }
       }
    }
@@ -193,7 +190,7 @@ int lz4ultra_expand_block(const unsigned char *pInBlock, int nBlockSize, unsigne
 
    while (pInBlock < pInBlockEnd) {
       const unsigned char token = *pInBlock++;
-      int nLiterals = (int)((unsigned int)((token & 0xf0) >> 4));
+      unsigned int nLiterals = (unsigned int)((token & 0xf0) >> 4);
 
       if (lz4ultra_expand_literals_slow(&pInBlock, pInBlockEnd, nLiterals, &pCurOutData, pOutDataEnd))
          return -1;
@@ -205,12 +202,14 @@ int lz4ultra_expand_block(const unsigned char *pInBlock, int nBlockSize, unsigne
          nMatchOffset |= (((unsigned int)*pInBlock++) << 8);
 
          const unsigned char *pSrc = pCurOutData - nMatchOffset;
-         if (pSrc < pOutData)
+         if (pSrc >= pOutData) {
+            unsigned int nMatchLen = (unsigned int)(token & 0x0f);
+            if (lz4ultra_expand_match_slow(&pInBlock, pInBlockEnd, pSrc, nMatchLen, &pCurOutData, pOutDataEnd, pOutDataFastEnd))
+               return -1;
+         }
+         else {
             return -1;
-
-         int nMatchLen = (int)((unsigned int)(token & 0x0f));
-         if (lz4ultra_expand_match_slow(&pInBlock, pInBlockEnd, pSrc, nMatchLen, &pCurOutData, pOutDataEnd, pOutDataFastEnd))
-            return -1;
+         }
       }
    }
 
